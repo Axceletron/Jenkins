ed9460516e794e8390f3c6e5b7cd1f3a6e58b4d0
Axceletron <rajprofessional1991@gmail.com>
2021-05-18 23:26:30 +0530

    Update test.tf

--- test.tf
+++ test.tf
@@ -1,7 +1,6 @@
 provider "aws" {
     region= "us-east-1"
-    shared_credentials_file = "/Users/rajaxceltron/.aws/credentials"
-    profile                 = "devuser"
+
 }
 
 data "aws_ami" "ubuntu" {
